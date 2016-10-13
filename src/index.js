51909da0d301043f760460cc3f0ebe5f275315de
john lam <j0hnlam114@gmail.com>
2016-10-13 15:01:26 -0700

    upload project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// let message = 'hello from entry j';
+// console.log(message);
+
+import React from 'react';
+import { render } from 'react-dom';
+import App from 'components/app';
+
+render(<App />, document.getElementById('app'))
